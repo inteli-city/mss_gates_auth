a0a87c81ff1a2feeb1f178564e0e07b885353031
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-10-09 19:17:28 -0300

    Update get_user_presenter.py

--- src/modules/get_user/app/get_user_presenter.py
+++ src/modules/get_user/app/get_user_presenter.py
@@ -1,10 +1,11 @@
+from src.shared.helpers.external_interfaces.http_codes import OK
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
 def lambda_handler(event, context):
     
     httpRequest = LambdaHttpRequest(data=event)
     httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
-    response = httpRequest.data['requester_user']
+    response = OK(httpRequest.data['requester_user'])
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
     
     return httpResponse.toDict()
